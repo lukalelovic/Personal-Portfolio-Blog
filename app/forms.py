4686d1d64330a7525a71c921b378de949424a221
lukalelovic <lukalelovic@gmail.com>
2019-12-10 22:10:16 +0000

    Initial commit

---
+++ app/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class PostForm(FlaskForm):
+    title = StringField('Enter Post Title:', validators=[DataRequired()])
+    post = TextAreaField('Enter Post Body:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
