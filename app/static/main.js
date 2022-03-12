de1298c0b7c2bed47ba5f3410b82accd7b5a0dd4
lukalelovic <lukalelovic@gmail.com>
2022-03-12 03:44:09 +0000

    bootstrap navbar pt 1

--- app/static/main.js
+++ app/static/main.js
@@ -1,6 +1,5 @@
 window.onscroll = function () { scrollFunction() };
-var nav = document.getElementById("navbar");
-var clicked = false;
+var nav = document.getElementById("nBar");
 
 function scrollFunction() {
     let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -9,31 +8,5 @@ function scrollFunction() {
         nav.style.top = "0";
     } else {
         nav.style.top = "-75px";
-    }
-
-    if (width > 1200) {
-        nav.style.height = "60px";
-        document.getElementById("navbar-right-align").style.visibility = "visible";
-        clicked = false;
-    } else if (clicked == false) {
-        document.getElementById("navbar-right-align").style.visibility = "hidden";
-    }
-
-    if (width < 1200) {
-        document.getElementById("navbar-right-align").style.visibility = "hidden";
-        nav.style.height = "60px";
-        if (clicked == true) 
-            clicked = false;
-    }
-}
-
-function dropDown() {
-    if (nav.style.height == "60px") {
-        nav.style.height = "25vw";
-        document.getElementById("navbar-right-align").style.visibility = "visible";
-        clicked = true;
-    } else {
-        nav.style.height = "60px";
-        document.getElementById("navbar-right-align").style.visibility = "hidden";
     }
 }
